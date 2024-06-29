5ea9da2d7637ba6f04ba7635f1f2d75f0f43d9ee
aapsi <aapsi001@gmail.com>
2024-06-30 02:53:19 +0530

    tokenfactory

---
+++ src/TokenFactory.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+contract TokenFactory {
+    function createToken(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) public returns (address) {
+        
+    }
+}
