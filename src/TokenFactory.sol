cbc2e19567c827a69fe67fdd981360ad2f790fc6
aapsi <aapsi001@gmail.com>
2024-06-30 18:20:21 +0530

    token

--- src/TokenFactory.sol
+++ src/TokenFactory.sol
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
+import "./Token.sol";
+
 contract TokenFactory {
-    function createToken(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) public returns (address) {
-        
+
+    uint256 public constant DECIMALS = 10 ** 18;
+    uint256 public constant MAX_SUPPLY = (10 ** 9) * DECIMALS;
+    uint256 public constant INITIAL_MINT = MAX_SUPPLY * 20 / 100;
+
+    mapping(address => bool) public tokens;
+
+    function createToken(string memory name, string memory ticker) public returns (address){
+        Token token = new Token(name, ticker, INITIAL_MINT);
+        tokens[address(token)] = true;
+        return address(token);
     }
 }
